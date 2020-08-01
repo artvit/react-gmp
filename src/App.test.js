dffcc2cea3b7be6d4a49e62ff527322d39b3040d
Artem Vitkovsky <artvitk@gmail.com>
2020-08-02 00:40:15 +0300

    Add config for tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('renders ', () => {
+    const { getAllByText } = render(<App />);
+    const helloElements = getAllByText(/hello/i);
+    expect(helloElements.length).toBe(5);
+  });
 });
