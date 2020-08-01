3b886221d514ca1e631e23cb56e5f8416d787232
Artem Vitkovsky <artvitk@gmail.com>
2020-08-02 02:30:22 +0300

    Add env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+
+module.exports = env => {
+  console.log('Production:', !!env.prod);
+  return {
+    mode: env.prod ? 'production' : 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
         }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html'
+      })
+    ],
+    optimization: {
+      minimizer: [new UglifyJsPlugin({ cache: true, parallel: true })]
+    }
+  };
 };
